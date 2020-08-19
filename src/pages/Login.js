4d6bc8b564b82e7fbb8a2c3aacc85bc82436ed0d
uday-kommoju-au8 <udaynagasivakumar@gmail.com>
2020-08-19 20:01:46 +0530

    login update

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -16,6 +16,8 @@ class Login extends Component {
     signInFlow: "popup",
     signInOptions: [
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+       firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
     ],
     callbacks: {
       signInSuccess: () => false
@@ -63,104 +65,3 @@ class Login extends Component {
 
 export default Login
 
-// import React from "react";
-// import {GoogleLogin} from "react-google-login";
-// import config from "../config";
-
-// const LoginPage = () =>{
-//     const responseGoogle = respone =>{
-//         console.log(respone);
-//     }
-//     return  (
-//         <div>
-//             <GoogleLogin
-//                clientId ={config.CLIENT_ID}
-//                buttonText= "Login"
-//                onSuccess = {responseGoogle}
-//                onFailure = {responseGoogle}
-//                scope = ""
-//                cookiePolicy = {"single_host_origin"}
-//             />
-//         </div>
-//     )
-// }
-// export default LoginPage;
-
-// import React, { useState } from "react";
-// import axios from "axios";
-// import { setUserSession } from "../Utils/Common";
-
-// function Login(props) {
-//   const [loading, setLoading] = useState(false);
-//   const username = useFormInput("");
-//   const password = useFormInput("");
-//   const [error, setError] = useState(null);
-
-//   // handle button click of login form
-//   const handleLogin = () => {
-//     setError(null);
-//     setLoading(true);
-//     axios
-//       .post("http://localhost:3000/users/signin", {
-//         username: username.value,
-//         password: password.value,
-//       })
-//       .then((response) => {
-//         setLoading(false);
-//         setUserSession(response.data.token, response.data.user);
-//         props.history.push("/profile");
-//       })
-//       .catch((error) => {
-//         setLoading(false);
-//         if (error.response.status === 401)
-//           setError(error.response.data.message);
-//         else setError("Something went wrong. Please try again later.");
-//       });
-//   };
-
-//   return (
-//     <div>
-//       Login
-//       <br />
-//       <br />
-//       <div>
-//         Username
-//         <br />
-//         <input type="text" {...username} />
-//       </div>
-//       <div style={{ marginTop: 10 }}>
-//         Password
-//         <br />
-//         <input type="password" {...password} />
-//       </div>
-//       {error && (
-//         <>
-//           <small style={{ color: "red" }}>{error}</small>
-//           <br />
-//         </>
-//       )}
-//       <br />
-//       <input
-//         type="button"
-//         value={loading ? "Loading..." : "Login"}
-//         onClick={handleLogin}
-//         disabled={loading}
-//       />
-//       <br />
-//     </div>
-//   );
-// }
-
-// const useFormInput = (initialValue) => {
-//   const [value, setValue] = useState(initialValue);
-
-//   const handleChange = (e) => {
-//     setValue(e.target.value);
-//   };
-//   return {
-//     value,
-//     onChange: handleChange,
-//   };
-// };
-
-// export default Login;
